ef6bd93e952ffb1234457aabedf794383cda4d6e
Nicolas Bigaouette <nbigaouette@gmail.com>
2014-02-15 00:14:08 -0500

    More verifications on arrays

--- unit_tests/simplesorts.cc
+++ unit_tests/simplesorts.cc
@@ -5,7 +5,74 @@
 #include "simplesorts.h"
 #include "printarray.h"
 
-//#define quiet
+#define quiet
+
+bool VerifyIfAllNotNaN(double const * const sorted, const int N)
+{
+    // Verify any NaN
+    for (int i = 1 ; i < N ; i++)
+    {
+        if (isnan(sorted[i]))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool VerifyIfOrdered(double const * const sorted, const int N)
+{
+    // Make sure array is ordered
+    for (int i = 1 ; i < N ; i++)
+    {
+        if (sorted[i] <= sorted[i-1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool VerifySortedUnique(double const * const original, double const * const sorted, const int N)
+{
+    // Make sure every element of the original array is present in the sorted one
+    // Make a temporary copy of the arrays
+    double *tmp_orig = new double[N];
+    double *tmp_sort = new double[N];
+    memcpy(tmp_orig, original, N*sizeof(double));
+    memcpy(tmp_sort, sorted,   N*sizeof(double));
+    // Flag every element of the (copied) arrays that match.
+    for (int i = 0 ; i < N ; i++)
+    {
+        for (int j = 0 ; j < N ; j++)
+        {
+            // Note that NAN != NAN
+            if (tmp_orig[i] == tmp_sort[j])
+            {
+                tmp_orig[i] = NAN;
+                tmp_sort[j] = NAN;
+                break;
+            }
+        }
+    }
+    // Now verify if one element of the temporary arrays is _not_ NaN.
+    // In that case, something went wrong!
+    for (int i = 0 ; i < N ; i++)
+    {
+        if (not isnan(tmp_orig[i]))
+        {
+            return false;
+        }
+        if (not isnan(tmp_sort[i]))
+        {
+            return false;
+        }
+    }
+    delete[] tmp_orig;
+    delete[] tmp_sort;
+
+    return true;
+}
 
 TEST(SimpleSorts, InsertionSort)
 {
@@ -47,6 +114,9 @@ TEST(SimpleSorts, InsertionSort)
     {
         EXPECT_NE(sorted_data[i], 0);
     }
+    ASSERT_TRUE(VerifyIfAllNotNaN(sorted_data, N));
+    ASSERT_TRUE(VerifyIfOrdered(sorted_data, N));
+    ASSERT_TRUE(VerifySortedUnique(to_sort_data, sorted_data, N));
 
     delete[] to_sort_data;
     delete[] sorted_data;
