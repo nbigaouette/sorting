99fdfb43cead190d3ea1f7e257f8eb465036a907
Nicolas Bigaouette <nbigaouette@gmail.com>
2014-02-14 23:45:50 -0500

    Insertion sort works (in place)

--- include/simplesorts-insertion-inl.h
+++ include/simplesorts-insertion-inl.h
@@ -1,11 +1,51 @@
 #ifndef SORTING_INCLUDE_SIMPLESORT_INSERTION_INL_H_
 #define SORTING_INCLUDE_SIMPLESORT_INSERTION_INL_H_
 
-template <class T>
-int InsertionSort(T const * const input, T * const output, const int N)
-{
+#include <cmath>
 
-    return 0;
+template <class T>
+void InsertionSort(T * const array, const int N)
+{
+    // Temporary value used during sorting
+    T tmp;
+
+    // Loop over all other elements
+    for (int i = 1 ; i < N ; i++)
+    {
+
+        // If the current element is smaller than the previous one, we
+        // go back in the array until we find the right place for the element.
+        if (array[i-1] > array[i])
+        {
+            // Temporarely store the element to sort
+            tmp = array[i];
+            array[i] = NAN;
+
+            // Go back in the array, starting with previous value.
+            for (int j = i-1 ; j >= 0 ; j--)
+            {
+                // If stored value is larger than array value, place stored value after
+                if (tmp >= array[j])
+                {
+                    array[j+1] = tmp;
+                    break;
+                }
+                else
+                {
+                    // Move element up
+                    array[j+1] = array[j];
+                    array[j] = NAN;
+                }
+            }
+            // If the first element is NaN, then the previous loop ended without
+            // finding a smaller number than the one stored. In that case, it is
+            // the smallest! Place it at the begining.
+            if (isnan(array[0]))
+            {
+                array[0] = tmp;
+            }
+        }
+    }
 }
 
 #endif  // SORTING_INCLUDE_SIMPLESORT_INSERTION_INL_H_
--- unit_tests/simplesorts.cc
+++ unit_tests/simplesorts.cc
@@ -1,45 +1,42 @@
-#include <stdlib.h> // rand()
-#include <time.h>   // time()
 #include <iostream>   // std::cout
 
 #include "gtest/gtest.h"
 
 #include "simplesorts.h"
+#include "printarray.h"
+
+//#define quiet
 
 TEST(SimpleSorts, InsertionSort)
 {
-    const int N = 4;
+    const int N = 5;
 
-    double *random_data = new double[N];
-    double *sorted_data = new double[N];
-    memset(sorted_data, 0, N*sizeof(double));
+    double *to_sort_data = new double[N];
+    double *sorted_data  = new double[N];
 
-    // Populate array with random data between 1 and 100
-    // Note that rand() is a BAD pseudo-random number generator (PRNG).
-    // Don't use it for anything serious!
-    srand(time(NULL));
+    to_sort_data[0] = 21.3;
+    to_sort_data[1] = 68.2;
+    to_sort_data[2] = 1.3;
+    to_sort_data[3] = 44.3;
+    to_sort_data[4] = 10.3;
+
     for (int i = 0 ; i < N ; i++)
     {
-        random_data[i] = rand() % 100 + 1;
+        sorted_data[i] = to_sort_data[i];
     }
 
-    /*
-    std::cout << "random_data:" << std::endl;
+    InsertionSort(sorted_data, N);
+
+#ifndef quiet
+    std::cout << "Arrays" << std::endl;
+    std::cout << "Index Original     Sorted " << std::endl;
     for (int i = 0 ; i < N ; i++)
     {
-        std::cout << i << "/" << N << "   " << random_data[i] << std::endl;
+        std::cout << i+1 << "/" << N << "   " 
+                    << to_sort_data[i] << "        " 
+                    << sorted_data[i] << std::endl;
     }
-    */
-
-  //  InsertionSort(random_data, sorted_data, N);
-
-//    /*
-    std::cout << "sorted_data:" << std::endl;
-    for (int i = 0 ; i < N ; i++)
-    {
-        std::cout << i << "/" << N << "   " << sorted_data[i] << std::endl;
-    }
-//    */
+#endif // quiet
 
     for (int i = 1 ; i < N ; i++)
     {
@@ -51,6 +48,6 @@ TEST(SimpleSorts, InsertionSort)
         EXPECT_NE(sorted_data[i], 0);
     }
 
-    delete[] random_data;
+    delete[] to_sort_data;
     delete[] sorted_data;
 }
