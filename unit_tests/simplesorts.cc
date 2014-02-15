200aae09d4e4d6d3bb3f6cd1a53a2cf926d78c0e
Nicolas Bigaouette <nbigaouette@gmail.com>
2014-02-14 22:02:08 -0500

    Actual unit test

--- unit_tests/simplesorts.cc
+++ unit_tests/simplesorts.cc
@@ -8,32 +8,47 @@
 
 TEST(SimpleSorts, InsertionSort)
 {
-    const int N = 10;
+    const int N = 4;
 
     double *random_data = new double[N];
     double *sorted_data = new double[N];
+    memset(sorted_data, 0, N*sizeof(double));
 
-    // Populate array with random data.
+    // Populate array with random data between 1 and 100
     // Note that rand() is a BAD pseudo-random number generator (PRNG).
     // Don't use it for anything serious!
     srand(time(NULL));
     for (int i = 0 ; i < N ; i++)
     {
-        random_data[i] = rand() % 100;
+        random_data[i] = rand() % 100 + 1;
     }
 
+    /*
     std::cout << "random_data:" << std::endl;
     for (int i = 0 ; i < N ; i++)
     {
         std::cout << i << "/" << N << "   " << random_data[i] << std::endl;
     }
+    */
 
-    InsertionSort(random_data, sorted_data, N);
+  //  InsertionSort(random_data, sorted_data, N);
 
-    std::cout << "random_data:" << std::endl;
+//    /*
+    std::cout << "sorted_data:" << std::endl;
     for (int i = 0 ; i < N ; i++)
     {
-        std::cout << i << "/" << N << "   " << random_data[i] << std::endl;
+        std::cout << i << "/" << N << "   " << sorted_data[i] << std::endl;
+    }
+//    */
+
+    for (int i = 1 ; i < N ; i++)
+    {
+        EXPECT_GE(sorted_data[i], sorted_data[i-1]);
+    }
+
+    for (int i = 0 ; i < N ; i++)
+    {
+        EXPECT_NE(sorted_data[i], 0);
     }
 
     delete[] random_data;
