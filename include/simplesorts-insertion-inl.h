df9ae2f7304f430af29e72295e6362b9042475e5
Nicolas Bigaouette <nbigaouette@gmail.com>
2014-02-14 21:38:48 -0500

    Initial (empty) headers for simple sorts (insertion)

---
+++ include/simplesorts-insertion-inl.h
@@ -0,0 +1,11 @@
+#ifndef SORTING_INCLUDE_SIMPLESORT_INSERTION_INL_H_
+#define SORTING_INCLUDE_SIMPLESORT_INSERTION_INL_H_
+
+template <class T>
+int InsertionSort(T const * const input, T * const output, const int N)
+{
+
+    return 0;
+}
+
+#endif  // SORTING_INCLUDE_SIMPLESORT_INSERTION_INL_H_
---
+++ include/simplesorts.h
@@ -0,0 +1,6 @@
+#ifndef SORTING_INCLUDE_SIMPLESORT_H_
+#define SORTING_INCLUDE_SIMPLESORT_H_
+
+#include "simplesorts-insertion-inl.h"
+
+#endif  // SORTING_INCLUDE_SIMPLESORT_H_
