8c15e0431b74a5629f5670779dd2da55e6587cc1
Nicolas Bigaouette <nbigaouette@gmail.com>
2014-02-16 08:28:31 -0500

    Initial structure for mergesort

---
+++ include/efficientsorts-merge-inl.h
@@ -0,0 +1,27 @@
+#ifndef SORTING_INCLUDE_EFFICIENTSORTS_MERGE_INL_H_
+#define SORTING_INCLUDE_EFFICIENTSORTS_MERGE_INL_H_
+
+namespace sorting {
+namespace efficient {
+
+template <class T>
+void MergeSort(T * const array, const int N)
+/**
+ * Efficient sorts: Merge sort
+ * Scaling:
+ *      Best case:  O(N log N)
+ *      Worst case: O(N log N)
+ * Useful:
+ *
+ */
+{
+    // Loop over all elements
+    for (int i = 0 ; i < N ; i++)
+    {
+    }
+}
+
+} // namespace efficient
+} // namespace sorting
+
+#endif  // SORTING_INCLUDE_EFFICIENTSORTS_MERGE_INL_H_
