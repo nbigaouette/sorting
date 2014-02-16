950ecf3ceee485328a08da8d05e21b5bbc29674a
Nicolas Bigaouette <nbigaouette@gmail.com>
2014-02-15 21:52:34 -0500

    New test: VerifyArray, VerifyIfOrdered

--- unit_tests/validate_verifyarray.cc
+++ unit_tests/validate_verifyarray.cc
@@ -26,3 +26,27 @@ TEST(VerifyArray, VerifyIfAllNotNaN)
     delete[] data;
 }
 
+TEST(VerifyArray, VerifyIfOrdered)
+{
+    const int N = 5;
+    double *data = new double[N];
+
+    data[0] = 1.3;
+    data[1] = 10.3;
+    data[2] = 21.3;
+    data[3] = 44.3;
+    data[4] = 68.2;
+
+    ASSERT_TRUE(VerifyIfOrdered(data, N));
+
+    data[0] = 21.3;
+    data[1] = 68.2;
+    data[2] = 1.3;
+    data[3] = 44.3;
+    data[4] = 10.3;
+
+    ASSERT_FALSE(VerifyIfOrdered(data, N));
+
+    delete[] data;
+}
+
