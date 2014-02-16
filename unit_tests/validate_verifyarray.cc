4510021b4775b956a97595115bc9e8f9cba57559
Nicolas Bigaouette <nbigaouette@gmail.com>
2014-02-15 21:49:11 -0500

    New test: VerifyArray, VerifyIfAllNotNaN

--- unit_tests/validate_verifyarray.cc
+++ unit_tests/validate_verifyarray.cc
@@ -6,3 +6,23 @@
 #include "printarray.h"
 #include "verifyarrays.h"
 
+TEST(VerifyArray, VerifyIfAllNotNaN)
+{
+    const int N = 5;
+    double *data = new double[N];
+
+    data[0] = 21.3;
+    data[1] = 68.2;
+    data[2] = 1.3;
+    data[3] = 44.3;
+    data[4] = 10.3;
+
+    ASSERT_TRUE(VerifyIfAllNotNaN(data, N));
+
+    data[3] = NAN;
+
+    ASSERT_FALSE(VerifyIfAllNotNaN(data, N));
+
+    delete[] data;
+}
+
