332f68d8815014c1b0da19a9d5298366f7b07038
Nicolas Bigaouette <nbigaouette@gmail.com>
2014-02-15 22:17:25 -0500

    Test selection sort

--- unit_tests/simplesorts.cc
+++ unit_tests/simplesorts.cc
@@ -137,3 +137,50 @@ TEST(SimpleSorts, InsertionSortWorstCase)
     delete[] sorted_data;
 
 }
+
+TEST(SimpleSorts, SelectionSort)
+{
+    const int N = 5;
+
+    double *to_sort_data = new double[N];
+    double *sorted_data  = new double[N];
+
+    to_sort_data[0] = 21.3;
+    to_sort_data[1] = 68.2;
+    to_sort_data[2] = 1.3;
+    to_sort_data[3] = 44.3;
+    to_sort_data[4] = 10.3;
+
+    memcpy(sorted_data, to_sort_data, N*sizeof(double));
+
+    InsertionSort(sorted_data, N);
+
+#ifndef quiet
+    std::cout << "Arrays" << std::endl;
+    std::cout << "Index Original     Sorted " << std::endl;
+    for (int i = 0 ; i < N ; i++)
+    {
+        std::cout << i+1 << "/" << N << "   "
+                    << to_sort_data[i] << "        "
+                    << sorted_data[i] << std::endl;
+    }
+#endif // quiet
+
+    for (int i = 1 ; i < N ; i++)
+    {
+        EXPECT_GE(sorted_data[i], sorted_data[i-1]);
+    }
+
+    for (int i = 0 ; i < N ; i++)
+    {
+        EXPECT_NE(sorted_data[i], 0);
+    }
+    ASSERT_TRUE(VerifyIfAllNotNaN(sorted_data, N));
+    ASSERT_TRUE(VerifyIfOrdered(sorted_data, N));
+    ASSERT_TRUE(VerifySortedUnique(to_sort_data, sorted_data, N));
+
+    delete[] to_sort_data;
+    delete[] sorted_data;
+}
+
+
