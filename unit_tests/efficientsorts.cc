a89ca02eaefae3442d797e182b83747094d8e7f9
Nicolas Bigaouette <nbigaouette@gmail.com>
2014-02-18 21:12:43 -0500

    Use VERIFY_SORTING_FIXED_SIZE_ARRAYS()

--- unit_tests/efficientsorts.cc
+++ unit_tests/efficientsorts.cc
@@ -9,23 +9,7 @@ TEST(EfficientSorts, MergeSortMultipleSizes)
     const double to_sorts[] = {6.0,   5.0,  3.0,  1.0,  2.4, 4.0, 10.0, 7.0,
                                3.42, 32.2, 44.2, 56.3, 67.9, 3.2, 44.2, 2.0};
 
-    for (int N = 1 ; N <= 16 ; N++)
-    {
-        double *to_sort_data = new double[N];
-        double *sorted_data  = new double[N];
-
-        for (int i = 0 ; i < N ; i++)
-        {
-            to_sort_data[i] = to_sorts[i];
-        }
-
-        memcpy(sorted_data, to_sort_data, N*sizeof(double));
-
-        SORT_AND_VERIFY(to_sort_data, sorted_data, N, sorting::efficient::MergeSort, false);
-
-        delete[] to_sort_data;
-        delete[] sorted_data;
-    }
+    VERIFY_SORTING_FIXED_SIZE_ARRAYS(16, to_sorts, sorting::efficient::MergeSort, false);
 }
 
 // *********************************************************************
