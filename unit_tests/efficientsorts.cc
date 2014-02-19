3304a76de8042e91218cb5c07b399760f5711002
Nicolas Bigaouette <nbigaouette@gmail.com>
2014-02-18 21:24:57 -0500

    New test: EfficientSorts, MergeSortKnownResult
    Sorting obtained with octave 3.8.0's sort().

--- unit_tests/efficientsorts.cc
+++ unit_tests/efficientsorts.cc
@@ -13,6 +13,17 @@ TEST(EfficientSorts, MergeSortMultipleSizes)
 }
 
 // *********************************************************************
+TEST(EfficientSorts, MergeSortKnownResult)
+{
+    const double to_sorts[] = {6.0,   5.0,  3.0,  1.0,  2.4, 4.0, 10.0, 7.0,
+                               3.42, 32.2, 44.2, 56.3, 67.9, 3.2, 44.2, 2.0};
+    const double known_sorted[] = { 1.0, 2.0,  2.4,  3.0,  3.2,  3.42,  4.0,  5.0,
+                                    6.0, 7.0, 10.0, 32.2, 44.2, 44.2,  56.3, 67.9};
+
+    VERIFY_SORTING_FIXED_SIZE_ARRAYS_AND_COMPARE(16, to_sorts, known_sorted, sorting::efficient::MergeSort, false);
+}
+
+// *********************************************************************
 TEST(EfficientSorts, MergeSortMultipleSizesRandom)
 {
     VERIFY_SORTING_RANDOM_ARRAYS(100, sorting::efficient::MergeSort, false)
