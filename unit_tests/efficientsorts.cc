95eca71a7365a8094accb79155570985ad2fa15c
Nicolas Bigaouette <nbigaouette@gmail.com>
2014-02-18 20:46:36 -0500

    Cleanup duplicated test

--- unit_tests/efficientsorts.cc
+++ unit_tests/efficientsorts.cc
@@ -12,36 +12,6 @@
 #define quiet
 
 // *********************************************************************
-TEST(EfficientSorts, MergeSort)
-{
-    const int N = 4;
-
-    double *to_sort_data = new double[N];
-    double *sorted_data  = new double[N];
-
-    // Initialize the array with random values between 1 and 100
-    // NOTE: rand() is a terrible pseudo-random number generator (PRNG).
-    // It is still used here for a simple testing task, but don't use it
-    // for anything serious.
-    srand(time(NULL));
-    for (int i = 0 ; i < N ; i++)
-    {
-        to_sort_data[i] = rand() % 100 + 1;
-    }
-    to_sort_data[0] = 6.0;
-    to_sort_data[1] = 5.0;
-    to_sort_data[2] = 3.0;
-    to_sort_data[3] = 1.0;
-
-    memcpy(sorted_data, to_sort_data, N*sizeof(double));
-
-    SORT_AND_VERIFY(to_sort_data, sorted_data, N, sorting::efficient::MergeSort, false);
-
-    delete[] to_sort_data;
-    delete[] sorted_data;
-}
-
-// *********************************************************************
 TEST(EfficientSorts, MergeSortMultipleSizes)
 {
     const double to_sorts[] = {6.0, 5.0, 3.0, 1.0, 2.4, 4.0, 10.0, 7.0};
