462b2c05ea813ecb05a0cede15f27c5a752308d0
Nicolas Bigaouette <nbigaouette@gmail.com>
2014-02-18 21:05:36 -0500

    New macro: VERIFY_RANDOM_ARRAYS()
    The new macro will create arrays of different sizes, up to Nmax, and
    put random values in them. It will then call SORT_AND_VERIFY() on the
    arrays.

--- unit_tests/efficientsorts.cc
+++ unit_tests/efficientsorts.cc
@@ -1,7 +1,3 @@
-#include <iostream>     // std::cout
-#include <stdlib.h>     // rand()
-#include <time.h>       // time()
-
 #include "gtest/gtest.h"
 
 #include "sorting.h"
@@ -35,26 +31,5 @@ TEST(EfficientSorts, MergeSortMultipleSizes)
 // *********************************************************************
 TEST(EfficientSorts, MergeSortMultipleSizesRandom)
 {
-    for (int N = 1 ; N < 100 ; N++)
-    {
-        double *to_sort_data = new double[N];
-        double *sorted_data  = new double[N];
-
-        // Initialize the array with random values between 1 and 100
-        // NOTE: rand() is a terrible pseudo-random number generator (PRNG).
-        // It is still used here for a simple testing task, but don't use it
-        // for anything serious.
-        srand(time(NULL));
-        for (int i = 0 ; i < N ; i++)
-        {
-            to_sort_data[i] = rand() % 100 + 1;
-        }
-
-        memcpy(sorted_data, to_sort_data, N*sizeof(double));
-
-        SORT_AND_VERIFY(to_sort_data, sorted_data, N, sorting::efficient::MergeSort, false);
-
-        delete[] to_sort_data;
-        delete[] sorted_data;
-    }
+    VERIFY_RANDOM_ARRAYS(100, sorting::efficient::MergeSort)
 }
--- unit_tests/verifysortedarrays.h
+++ unit_tests/verifysortedarrays.h
@@ -1,6 +1,9 @@
 #ifndef SORTING_UNIT_TESTS_VERIFYSORTEDARRAYS_H_
 #define SORTING_UNIT_TESTS_VERIFYSORTEDARRAYS_H_
 
+#include <stdlib.h>     // rand()
+#include <time.h>       // time()
+
 #include "gtest/gtest.h"
 
 #include "verifyarrays.h"
@@ -35,5 +38,29 @@
                                                                         \
     VALIDATE_SORTING(to_sort_data, sorted_data, N);                     \
 }
+
+#define VERIFY_RANDOM_ARRAYS(Nmax, SortingFunction)                     \
+    for (int N = 1 ; N < Nmax ; N++)                                    \
+    {                                                                   \
+        double *to_sort_data = new double[N];                           \
+        double *sorted_data  = new double[N];                           \
+                                                                        \
+        /* Initialize the array with random values between 1 and 100    \
+           NOTE: rand() is a terrible pseudo-random number generator (PRNG). \
+           It is still used here for a simple testing task, but don't use it \
+           for anything serious. */                                     \
+        srand(time(NULL));                                              \
+        for (int i = 0 ; i < N ; i++)                                   \
+        {                                                               \
+            to_sort_data[i] = rand() % 100 + 1;                         \
+        }                                                               \
+                                                                        \
+        memcpy(sorted_data, to_sort_data, N*sizeof(double));            \
+                                                                        \
+        SORT_AND_VERIFY(to_sort_data, sorted_data, N, SortingFunction, false); \
+                                                                        \
+        delete[] to_sort_data;                                          \
+        delete[] sorted_data;                                           \
+    }
 
 #endif  // SORTING_UNIT_TESTS_VERIFYSORTEDARRAYS_H_
