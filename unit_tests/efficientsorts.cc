48b3bb8ae63fb5bc8c8f9313ab3ef3f543cb9b7f
Nicolas Bigaouette <nbigaouette@gmail.com>
2014-02-18 20:57:19 -0500

    Array sizes from 1 to 16

--- unit_tests/efficientsorts.cc
+++ unit_tests/efficientsorts.cc
@@ -10,9 +10,10 @@
 // *********************************************************************
 TEST(EfficientSorts, MergeSortMultipleSizes)
 {
-    const double to_sorts[] = {6.0, 5.0, 3.0, 1.0, 2.4, 4.0, 10.0, 7.0};
+    const double to_sorts[] = {6.0,   5.0,  3.0,  1.0,  2.4, 4.0, 10.0, 7.0,
+                               3.42, 32.2, 44.2, 56.3, 67.9, 3.2, 44.2, 2.0};
 
-    for (int N = 1 ; N < 8 ; N++)
+    for (int N = 1 ; N <= 16 ; N++)
     {
         double *to_sort_data = new double[N];
         double *sorted_data  = new double[N];
