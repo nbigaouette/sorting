5ef27bdac624e917161f0ed9f120e9d1290a0db8
Nicolas Bigaouette <nbigaouette@gmail.com>
2014-02-25 21:25:32 -0500

    Initial (empty) implementation for Quicksort

---
+++ include/efficientsorts-quick-inl.h
@@ -0,0 +1,15 @@
+#ifndef SORTING_INCLUDE_EFFICIENTSORTS_QUICK_INL_H_
+#define SORTING_INCLUDE_EFFICIENTSORTS_QUICK_INL_H_
+
+namespace sorting {
+namespace efficient {
+
+template <class T>
+void Quicksort(T * const array, const int N)
+{
+}
+
+} // namespace efficient
+} // namespace sorting
+
+#endif // #ifndef SORTING_INCLUDE_EFFICIENTSORTS_QUICK_INL_H_
--- include/efficientsorts.h
+++ include/efficientsorts.h
@@ -2,5 +2,6 @@
 #define SORTING_INCLUDE_EFFICIENTSORTS_H_
 
 #include "efficientsorts-merge-inl.h"
+#include "efficientsorts-quick-inl.h"
 
 #endif  // SORTING_INCLUDE_EFFICIENTSORTS_H_
