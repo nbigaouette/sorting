bdbbcc0baca6c1ba054f6e519d343015f7b4003c
Nicolas Bigaouette <nbigaouette@gmail.com>
2014-11-08 10:43:13 -0500

    Optimization for bubble sort

--- include/bubblesorts-bubble-inl.h
+++ include/bubblesorts-bubble-inl.h
@@ -18,13 +18,15 @@ void BubbleSort(T * const array, const int N)
     int pass_count = 0;  // Number of pass over the array.
     int swap_count = 0;  // Number of swap for a single pass.
 
+    int loop_N = N;
+
     // Pass over the array while the swap count is non-null.
     do
     {
         // It's a new pass; reset the count of swap.
         swap_count = 0;
         // Iterate over the array, skipping the last item
-        for (int i = 0 ; i < N-1 ; i++)
+        for (int i = 0 ; i < loop_N-1 ; i++)
         {
             // Swap elements if next one is "smaller" and register the swap.
             if (array[i] > array[i+1])
@@ -34,6 +36,10 @@ void BubbleSort(T * const array, const int N)
             }
         }
         pass_count++;
+
+        // Optimization: After M pass, the Mth element is at its final position
+        loop_N--;
+
     } while (swap_count != 0);
 }
 
