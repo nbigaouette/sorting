61ac8f024179caca0497ffb5fd9aa64da94ee574
Nicolas Bigaouette <nbigaouette@gmail.com>
2014-11-08 11:19:18 -0500

    Basic profiling

--- profiling/main.cc
+++ profiling/main.cc
@@ -1,5 +1,94 @@
+#include <iostream>
+#include "sorting.h"
+#include "printarray.h"
+
+#include <vector>
+#include <cmath>
+#include <random>
+#include <functional>
+#include <chrono>
 
 
-int main (int argc, char *argv[])
+double profileAverage_ms(const int *const data, const int N, const int avgN, void (*sort)(int * const, const int))
 {
+    int *array = new int[N];
+
+    std::vector<double> diffs(avgN);
+    //double avg = 0.0;
+
+    for (int j = 0 ; j < avgN ; j++)
+    {
+        memcpy(array, data+j, N*sizeof(int));
+
+        //PrintArray(array, N);
+
+        auto start = std::chrono::steady_clock::now();
+        (*sort)(array, N);
+        auto stop  = std::chrono::steady_clock::now();
+
+        //PrintArray(array, N);
+
+        auto diff = stop - start;
+        diffs[j] = std::chrono::duration <double, std::milli> (diff).count();
+    }
+
+    double avg = 0.0;
+    for (int j = 0 ; j < avgN ; j++)
+    {
+        avg += diffs[j] / double(avgN);
+    }
+    double stddev = 0.0;
+    for (int j = 0 ; j < avgN ; j++)
+    {
+        stddev += (diffs[j] - avg)*(diffs[j] - avg) / double(avgN);
+    }
+    stddev = std::sqrt(stddev);
+
+    std::cout << N << ", " << avg << " +- " << stddev << " ms" << std::endl;
+
+    delete[] array;
+
+    return avg;
+}
+
+
+void profileSort(void (*sort)(int * const, const int))
+{
+    const int pow2min = 0;
+    const int pow2max = 16;
+
+    const int seed = 4;
+
+    std::uniform_int_distribution<int> distribution(0, 99);
+    std::mt19937 engine(seed); // Mersenne twister MT19937
+    auto generator = std::bind(distribution, engine);
+
+
+    // Allocate more than required; profileAverage() will copy the data
+    // with an offset.
+    int *data = new int[2 << (pow2max+1)];
+    for (int i = 0 ; i < 2 << (pow2max+1) ; i++)
+    {
+        data[i] = generator();
+    }
+
+
+    //PrintArray(Ns.data(), Ns.size());
+
+    for (int j = pow2min ; j <= pow2max ; j++)
+    {
+        const int N = 2 << j;
+        profileAverage_ms(data, N, 6, sort);
+        //std::cout << j << ", " << N << ", " << profileAverage_ms(data, N, 6, sort) << " ms" << std::endl;
+    }
+
+    delete[] data;
+}
+
+int main(int argc, char *argv[])
+{
+    std::cout << "main()" << std::endl;
+
+    //profileSort(&sorting::simple::InsertionSort);
+    profileSort(&sorting::efficient::Quicksort);
 }
