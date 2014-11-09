9e643a744086b1c5e26355b344c3cf86b32325ac
Nicolas Bigaouette <nbigaouette@gmail.com>
2014-11-08 21:13:02 -0500

    Plot in seconds, not milliseconds. Also clean up the file name for legend.

--- profiling/plot.py
+++ profiling/plot.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import glob
+import re
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,16 +15,24 @@ ax = fig.add_subplot(111)
 
 colors = iter(plt.cm.rainbow(np.linspace(0,1,len(csv_files))))
 
+p = re.compile(r'profiling_(.*?)_(.*?)\.csv')
+ms_to_s = 1.0 / 1000.0
+
 for csv_file in csv_files:
     data = np.genfromtxt(csv_file, delimiter=',', skip_header=1).transpose()
     j   = data[0]
     N   = data[1]
     avg = data[2]
     std = data[3]
-    ax.errorbar(N, avg, yerr=std, label=csv_file, color=next(colors), marker='o')
+    m = p.search(csv_file)
+    name = m.group(2)
+    name = name.replace('_', ' ')
+    ax.errorbar(N, avg*ms_to_s, yerr=std*ms_to_s, 
+                label=name, color=next(colors), marker='o')
 
+ax.grid(True)
 ax.set_xlabel('N')
-ax.set_ylabel('Timing [ms]')
+ax.set_ylabel('Timing [s]')
 ax.legend(loc='best')
 
 plt.show()
